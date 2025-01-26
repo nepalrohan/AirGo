d04abdbf6dff61ef19ba2ee66caa1712385b0156
nepalrohan <rohannepal098@gmail.com>
2025-01-26 14:30:08 +0545

    add relation betwn city and airport

---
+++ src/migrations/20250125162104-update-city-airport-association.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports',{
+      type: 'FOREIGN KEY',
+      name:'city_fkey_constraints_name',
+      fields:['cityId'],
+
+      references: {
+        table: 'cities',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports', 'city_fkey_constraints_name');
+  }
+};
+
+
+
+
